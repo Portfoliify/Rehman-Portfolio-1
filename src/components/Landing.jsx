8ef4152ac7ba2b85ffc79474e91154e626dda659
Portfoliify <tnmadgaming9920@gmail.com>
2023-12-26 20:59:12 +0530

    first commit

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -41,15 +41,15 @@ const Landing = () => {
   const about2ImageSource2 = pic10;
   //Content
 
-        const emailAddress = "sample@gmail.com";
-        const navName = "Rehman sha";
-        const about1FullParagraph = "Hi I am Mohan, As a photographer I am dedicated to transforming ordinary moments into extraordinary memories.";
-        const about2HeadingFullText = "My Lens, Your Story";
+        const emailAddress = "rehmnshss@gmail.com";
+        const navName = "Rehman shaa";
+        const about1FullParagraph = "Hi I am $Mohan, As a photographer I am dedicated to transforming ordinary moments into extraordinary memories.";
+        const about2HeadingFullText = "My $Lens, Your Story";
         const about2Paragraph = "With a passion for capturing fleeting moments and transforming them into lasting memories, I embark on a visual journey, guided by the light and emotions that surround me. My photography transcends mere images, weaving together narratives that resonate with viewers on a deeper level. Whether it's the raw energy of a live performance, the quiet intimacy of a wedding, or the timeless beauty of a landscape, I strive to create images that reflect the essence of the subject and evoke a sense of connection.";  
         const about3Paragraph = "If you are looking to discuss a project or just talk photography";
-        const linkedInLink = "https://linkedin.com";
+        const linkedInLink = "https://instagram.com";
         const instagramLink = "https://instagram.com";
-        const twitterLink = "https://twitter.com";
+        const twitterLink = "https://instagram.com";
 //Content
   let about2HeadingHighlight = "",
     about2HeadingTextBefore = about2HeadingFullText,
